06eef9db45171a686cf5921cd7c4a65d2e1fae5f
ak <a.kemalakcay@gmail.com>
2024-08-18 02:29:34 +0300

    use `DynamicIcon` in `NetworkIcon`, `WalletIcon`, `TokenIcon`

--- packages/react/src/NetworkIcon.tsx
+++ packages/react/src/NetworkIcon.tsx
@@ -1,77 +1,21 @@
-import { forwardRef, ReactElement, useState, useEffect } from 'react'
-import { networks } from '@web3icons/core/metadata'
-import { INetworkMetadata } from '@web3icons/core'
+import { forwardRef } from 'react'
 import { NetworkIconProps } from './types'
-import { toKebabCase, toPascalCase, NETWORK_ICON_IMPORT_MAP } from './utils'
-
-const findNetwork = (network: string): INetworkMetadata | undefined => {
-  const networkObj = networks.find(
-    (net) =>
-      net.id.toLowerCase() === toKebabCase(network) ||
-      net.name.toLowerCase() === network.toLowerCase() ||
-      net.shortname?.toLowerCase() === network.toLowerCase(),
-  )
-  return networkObj
-}
-
-const DynamicIconLoader = forwardRef<SVGSVGElement, NetworkIconProps>(
-  (
-    { network, size, className, variant, color }: NetworkIconProps,
-    ref,
-  ): ReactElement | null => {
-    const [IconComponent, setIconComponent] = useState<ReactElement | null>(
-      null,
-    )
-
-    useEffect(() => {
-      const loadIcon = async () => {
-        const matchedNetwork = findNetwork(network)
-        if (!matchedNetwork) {
-          console.error(`Network not found: ${network}`)
-          return
-        }
-
-        const iconName = `Network${toPascalCase(matchedNetwork.name)}`
-        const importFunction =
-          NETWORK_ICON_IMPORT_MAP[iconName] ??
-          NETWORK_ICON_IMPORT_MAP[`Network${toPascalCase(matchedNetwork.id)}`]
-
-        if (importFunction) {
-          try {
-            const { default: ImportedIcon } = await importFunction()
-            setIconComponent(
-              <ImportedIcon
-                ref={ref}
-                network={network}
-                size={size}
-                color={color}
-                className={className}
-                variant={variant}
-              />,
-            )
-          } catch (error) {
-            console.error(`Error loading icon: ${iconName}`, error)
-          }
-        }
-      }
-
-      loadIcon()
-    }, [network, ref, size, className, variant, color])
-
-    return IconComponent
-  },
-)
+import { findNetwork } from './utils'
+import { DynamicIcon } from './DynamicIcon'
 
 export const NetworkIcon = forwardRef<SVGSVGElement, NetworkIconProps>(
-  ({ network, size, className, variant = 'mono', color }, ref) => {
+  ({ network, size, className, variant = 'mono', color, fallback }, ref) => {
+    const metadata = findNetwork({ network })
     return (
-      <DynamicIconLoader
-        network={network}
+      <DynamicIcon
+        type="network"
+        metadata={metadata}
         size={size}
         className={className}
         variant={variant}
         color={color}
         ref={ref}
+        fallback={fallback}
       />
     )
   },
--- packages/react/src/TokenIcon.tsx
+++ packages/react/src/TokenIcon.tsx
@@ -1,117 +1,7 @@
-import { forwardRef, ReactElement, useEffect, useState } from 'react'
-import { ITokenMetadata, tokens } from '@web3icons/core'
+import { forwardRef } from 'react'
 import { TokenIconProps } from './types'
-import { TOKEN_ICON_IMPORT_MAP } from './utils'
-
-const findToken = (
-  symbol?: string,
-  address?: string,
-  network?: string,
-): ITokenMetadata | undefined => {
-  if (symbol) {
-    return tokens.find(
-      (token) => token.symbol.toLowerCase() === symbol.toLowerCase(),
-    )
-  } else if (address && network) {
-    return tokens.find(
-      (token) =>
-        token.addresses[network]?.toLowerCase() === address.toLowerCase(),
-    )
-  }
-  return undefined
-}
-
-const DynamicIconLoader = forwardRef<SVGSVGElement, TokenIconProps>(
-  (
-    {
-      symbol,
-      size,
-      className,
-      variant,
-      color,
-      address,
-      network,
-      defaultImg,
-    }: TokenIconProps,
-    ref,
-  ): ReactElement | null => {
-    const [IconComponent, setIconComponent] = useState<ReactElement | null>(
-      null,
-    )
-
-    useEffect(() => {
-      const loadIcon = async () => {
-        const tokenData = findToken(symbol, address, network)
-
-        if (!tokenData) {
-          setIconComponent(
-            <img
-              ref={ref as React.RefObject<HTMLImageElement>}
-              src={defaultImg}
-              alt="default icon"
-              className={className}
-              style={{ width: size, height: size }}
-            />,
-          )
-          return
-        }
-
-        const iconName = `Token${tokenData.symbol.toUpperCase()}`
-        const importFunction = TOKEN_ICON_IMPORT_MAP[iconName]
-
-        if (importFunction) {
-          try {
-            const { default: ImportedIcon } = await importFunction()
-            setIconComponent(
-              <ImportedIcon
-                ref={ref}
-                symbol={tokenData.symbol}
-                size={size}
-                color={color}
-                className={className}
-                variant={variant}
-              />,
-            )
-          } catch (error) {
-            console.error(`Error loading icon: ${iconName}`, error)
-            setIconComponent(
-              <img
-                ref={ref as React.RefObject<HTMLImageElement>}
-                src={defaultImg}
-                alt="default icon"
-                className={className}
-                style={{ width: size, height: size }}
-              />,
-            )
-          }
-        } else {
-          setIconComponent(
-            <img
-              ref={ref as React.RefObject<HTMLImageElement>}
-              src={defaultImg}
-              alt="defult icon"
-              className={className}
-              style={{ width: size, height: size }}
-            />,
-          )
-        }
-      }
-      loadIcon()
-    }, [
-      symbol,
-      address,
-      network,
-      ref,
-      size,
-      className,
-      variant,
-      color,
-      defaultImg,
-    ])
-
-    return IconComponent
-  },
-)
+import { findToken } from './utils'
+import { DynamicIcon } from './DynamicIcon'
 
 export const TokenIcon = forwardRef<SVGSVGElement, TokenIconProps>(
   (
@@ -123,28 +13,23 @@ export const TokenIcon = forwardRef<SVGSVGElement, TokenIconProps>(
       color,
       address,
       network,
-      defaultImg,
+      fallback,
     },
     ref,
   ) => {
-    const props = {
-      color,
-      size,
-      variant,
-      className,
-      ref,
-      defaultImg,
-    }
-
-    if (symbol) {
-      return <DynamicIconLoader symbol={symbol} {...props} />
-    }
-
-    if (address && network) {
-      return (
-        <DynamicIconLoader address={address} network={network} {...props} />
-      )
-    }
+    const metadata = findToken(symbol ? { symbol } : { address, network })
+    return (
+      <DynamicIcon
+        type="token"
+        metadata={metadata}
+        className={className}
+        color={color}
+        fallback={fallback}
+        ref={ref}
+        size={size}
+        variant={variant}
+      />
+    )
   },
 )
 
