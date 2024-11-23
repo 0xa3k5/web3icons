54080561faa56243a2fbf20b0866b1293cef965e
ak <a.kemalakcay@gmail.com>
2024-11-23 16:40:04 +0300

    separate metadata generation script

--- packages/utils/src/build-scripts/core/core.pre-build.ts
+++ packages/utils/src/build-scripts/core/core.pre-build.ts
@@ -1,12 +1,10 @@
 import { generateIndex } from './core-generate-index'
-import { generateMetadataTs } from './core-generate-metadata'
 import { generateSvgModule } from './core-generate-svgs-module'
 import { optimizeSVGs } from './core-optimize-svgs'
 
 function preBuild() {
   optimizeSVGs()
   generateSvgModule()
-  generateMetadataTs()
   generateIndex()
 }
 
