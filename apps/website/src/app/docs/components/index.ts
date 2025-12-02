a9787e8ccc349628cef7eb1b62fc4c0521b8a2b8
ak <a.kemalakcay@gmail.com>
2025-12-02 11:54:52 +0300

    feat(docs): framework guides

--- apps/website/src/app/docs/components/index.ts
+++ apps/website/src/app/docs/components/index.ts
@@ -1,6 +1,32 @@
 export { LiveExample } from './live-example'
-export { WalletSelector, ExchangeList } from './docs-examples'
 export { InstallTabs } from './install-tabs'
 export { IndividualReactIcons } from './individual-react-icons'
 export { DynamicReactIcons } from './dynamic-react-icons'
 export { SizeExample } from './size-example'
+export {
+  DynamicTokenLookup,
+  DynamicNetworkLookup,
+  DynamicWalletLookup,
+  DynamicExchangeLookup,
+} from './dynamic-lookup'
+export {
+  TokenListWithNetwork,
+  TokenSwapPreview,
+  NetworkSelector,
+  NetworkDropdown,
+  NetworkPills,
+  ExchangeList,
+  TokenList,
+  TokenListWithNetworkExample,
+  TokenSwapExample,
+  TokenSelectorExample,
+  TokenAddressExample,
+  TokenStaticExample,
+  NetworkSelectorBasic,
+  NetworkDropdownExample,
+  NetworkGridExample,
+  NetworkStatusExample,
+  NetworkWagmiExample,
+  NetworkStaticExample,
+} from './recipe-examples'
+export { WalletSelector, WalletSelectorWagmi } from './wallet-recipes'
---
+++ apps/website/src/app/docs/guides/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function GuidesPage() {
+  redirect('/docs/guides/nextjs')
+}
